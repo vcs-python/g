11f4ffaef3eb79ba4b8a92144d8b574d741e2cfc
Tony Narlock <tony@git-pull.com>
2022-08-17 01:31:45 +0200

    Tag v0.0.1

--- g/__about__.py
+++ g/__about__.py
@@ -1,7 +1,7 @@
 __title__ = "g"
 __package_name__ = "g"
 __description__ = "cli shortcut for directory's vcs command: git, svn, hg"
-__version__ = "0.0.0a1"
+__version__ = "0.0.1"
 __author__ = "Tony Narlock"
 __github__ = "https://github.com/vcs-python/g"
 __docs__ = "https://g.git-pull.com"

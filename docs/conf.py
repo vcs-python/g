087809763e02cd76f509d8703500d76bd5c8c8d3
Tony Narlock <tony@git-pull.com>
2022-09-29 19:23:09 -0500

    docs(conf): Update issue_url_tpl

--- docs/conf.py
+++ docs/conf.py
@@ -96,7 +96,7 @@ html_sidebars = {
 }
 
 # linkify_issues
-issue_url_tpl = "https://github.com/vcs-python/g/issues/{issue_id}"
+issue_url_tpl = about["__github__"] + "/issues/{issue_id}"
 
 # sphinx.ext.autodoc
 toc_object_entries_show_parents = "hide"
